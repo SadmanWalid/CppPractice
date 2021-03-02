ae5afab5a13c11eb05c4a34f68a4f267fb92a87b
Sadman Walid <40543547+SadmanWalid@users.noreply.github.com>
2021-03-02 09:02:17 +0600

    Adding <=,>=,== functionalities.

--- Practices from cppds/fractionClass/fractionClass.cpp
+++ Practices from cppds/fractionClass/fractionClass.cpp
@@ -5,11 +5,7 @@ using namespace std;
 
 
 
-
-
-
 int gcf(int top, int bottom) //find greatest common factor between two intiger numbers. Euclid’s Algorithm states that the greatest common divisor of two integers m and n is n if n divides m evenly. However, if n does not divide m evenly, then the answer is the greatest common divisor of n and the remainder of m divided by n. 
-
 {
     while (top % bottom != 0)
     {
@@ -50,7 +46,10 @@ public:
     friend Fraction operator-(const Fraction frac1, const Fraction frac2); //friend function to overload operator -
     friend Fraction operator*(const Fraction frac1, const Fraction frac2); //friend function to overload operator *
     friend Fraction operator/(const Fraction frac1, const Fraction frac2); //friend function to overload operator /
-};
+    friend void operator==(const Fraction frac1, const Fraction frac2);//friend function to overload operator == to compare is two function are equal
+    friend void operator>=(const Fraction frac1,const Fraction frac2); // to overload >= operator
+    friend void operator<=(const Fraction frac1,const Fraction frac2); // to overload <= operator
+}; 
 ostream &operator<<(ostream &display, const Fraction frac) // operator << overloading
 {
     display << "Fraction :" << frac.num << "/" << frac.de;
@@ -59,44 +58,79 @@ ostream &operator<<(ostream &display, const Fraction frac) // operator << overlo
 
 Fraction operator+(const Fraction frac1, const Fraction frac2) //operator + overloading
 {
-    Fraction newFrac;
-    newFrac.num = frac1.num * frac2.de + frac1.de * frac2.num;
-    newFrac.de = frac1.de * frac2.de;
-    return newFrac;
+    
+    int newNum= frac1.num * frac2.de + frac1.de * frac2.num;
+    int newDe = frac1.de * frac2.de;
+    return Fraction(newNum,newDe);
 }
 
 Fraction operator-(const Fraction frac1, const Fraction frac2) //operator - overloading
 {
-    Fraction newFrac;
-    newFrac.num = frac1.num * frac2.de - frac1.de * frac2.num;
-    newFrac.de = frac1.de * frac2.de;
-    return newFrac;
+    
+    int newNum = frac1.num * frac2.de - frac1.de * frac2.num;
+    int newDe = frac1.de * frac2.de;
+    return Fraction(newNum,newDe);
 }
-
 Fraction operator*(const Fraction frac1, const Fraction frac2) //operator * overloading
 {
-    Fraction newFrac;
-    newFrac.num = frac1.num * frac2.num;
-    newFrac.de = frac1.de * frac2.de;
-    return newFrac;
+   
+    int newNum = frac1.num * frac2.num;
+    int newDe = frac1.de * frac2.de;
+    return Fraction(newNum,newDe);
 }
 
 Fraction operator/(const Fraction frac1, const Fraction frac2) //operator / overloading
 {
-    Fraction newFrac;
-    newFrac.num = frac1.num * frac2.de;
-    newFrac.de = frac1.de * frac2.num;
-    return newFrac;
+   
+    int newNum = frac1.num * frac2.de;
+    int newDe = frac1.de * frac2.num;
+    return Fraction(newNum,newDe);
+}
+void operator==(const Fraction frac1, const Fraction frac2) // operator == overloading
+{
+  int num1=frac1.num*frac2.de;
+  int num2=frac2.num*frac1.de;
+  if(num1==num2)
+   cout<<"they are equal"<<endl;
+  else
+   cout<<"They are not equal"<<endl;
 }
 
+void operator>=(const Fraction frac1,const Fraction frac2)// operator >= overloading
+{
+    int fracOne=frac1.num*frac2.de;
+    int fracTwo=frac2.num*frac1.de;
+
+    if (fracOne>fracTwo)
+      cout<<"Fraction "<<frac1.num<<"/"<<frac1.de<<" is bigger than "<<frac2.num<<"/"<<frac2.de<<endl;
+    else if (fracTwo>fracOne)
+      cout<<"Fraction "<<frac2.num<<"/"<<frac2.de<<" is bigger than "<<frac1.num<<"/"<<frac1.de<<endl;
+    else
+       cout<<"They are equal"<<endl;
+}
+void operator<=(const Fraction frac1,const Fraction frac2)// operator <= overloading
+{
+    int fracOne=frac1.num*frac2.de;
+    int fracTwo=frac2.num*frac1.de;
+
+    if (fracOne<fracTwo)
+      cout<<"Fraction "<<frac1.num<<"/"<<frac1.de<<" is smaller than "<<frac2.num<<"/"<<frac2.de<<endl;
+    else if (fracTwo<fracOne)
+      cout<<"Fraction "<<frac2.num<<"/"<<frac2.de<<" is smaller than "<<frac1.num<<"/"<<frac1.de<<endl;
+    else
+       cout<<"They are equal"<<endl;
+}
 int main() //main function
 {
-    Fraction fr1(12, 9);
-    Fraction fr2(3, 4);
+    Fraction fr1(5, 15);
+    Fraction fr2(5, 15);
     Fraction fr3;
     //fr3=fr3.addition(fr1,fr2);
-    fr3 = fr1 * fr2;
-    cout << fr3;
+    fr3 = fr1 /fr2;
+    cout << fr3<<endl;
+    fr1<=fr2;
+    fr1>=fr2;
+    
 
     return 0;
 }
